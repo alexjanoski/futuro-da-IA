b25a44a814c34ce14e9dbd0abb4dfc28f3d0b3fe
alexjanoski <145134180+alexjanoski@users.noreply.github.com>
2025-08-14 11:46:43 -0300

    dfghjkl

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
 
